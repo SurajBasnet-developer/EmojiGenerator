48710fe46f3196e471216f21cefd0470ed47f45c
Suraj <surajbasnet449@gmail.com>
2023-03-02 10:23:20 +0545

    firts commit

---
+++ src/EmojiContainer.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import "./App.css";
+const emojis = [
+  "😀",
+  "😃",
+  "😄",
+  "😁",
+  "😆",
+  "😅",
+  "🤣",
+  "😂",
+  "🙂",
+  "🙃",
+  "😉",
+  "😊",
+  "😇",
+  "🥰",
+  "😍",
+  "🤩",
+  "😘",
+  "😗",
+  "😚",
+  "😙",
+  "😋",
+  "😛",
+  "😝",
+  "😜",
+  "🤪",
+  "🤨",
+  "🧐",
+  "🤓",
+  "😎",
+  "🤩",
+  "🥳",
+  "😏",
+  "😒",
+  "😞",
+  "😔",
+  "😟",
+  "😕",
+  "🙁",
+  "☹️",
+  "😣",
+  "😖",
+  "😫",
+  "😩",
+  "🥺",
+  "😢",
+  "😭",
+  "😤",
+  "😠",
+  "😡",
+  "🤬",
+  "🤯",
+  "😳",
+  "🥵",
+  "🥶",
+  "😱",
+  "😨",
+  "😰",
+  "😥",
+  "😓",
+  "🤗",
+  "🤔",
+  "🤭",
+  "🤫",
+  "🤥",
+  "😶",
+  "😐",
+  "😑",
+  "😬",
+  "🙄",
+  "😯",
+  "😦",
+  "😧",
+  "😮",
+  "😲",
+  "😴",
+  "🤤",
+  "😪",
+  "😵",
+  "🤐",
+  "🥴",
+  "🤢",
+  "🤮",
+  "🤕",
+  "🥳",
+  "😷",
+  "🤒",
+  "🤕",
+  "🤑",
+  "🤠",
+  "😈",
+  "👿",
+  "👹",
+  "👺",
+  "💀",
+  "👻",
+  "👽",
+  "🤖",
+  "💩",
+  "😺",
+  "😸",
+  "😹",
+  "😻",
+  "😼",
+  "😽",
+  "🙀",
+  "😿",
+  "😾",
+];
+const RandomEmojiGenerator = () => {
+  const [emoji, setEmoji] = useState("");
+  function generate() {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    setEmoji(emojis[randomIndex]);
+  }
+  return (
+    <>
+      <div style={{ fontSize: "30px", textAlign: "center" }}>
+        <h1>Random Emoji Generator</h1>
+        <Stack spacing={2} direction="center">
+          <Button onClick={generate} variant="contained">
+            Generate Emoji
+          </Button>
+        </Stack>
+        <h3> {emoji && <p>{emoji}</p>} </h3>
+      </div>
+    </>
+  );
+};
+export default RandomEmojiGenerator;
